f284b74f704d5d158185c5b37fded890bcc1c7ef
Maciej Modelski <maciej.modelski@dfinity.org>
2023-08-28 15:20:05 +0000

    fix comment

--- test/suite/lib.rs
+++ test/suite/lib.rs
@@ -373,7 +373,7 @@ pub async fn icrc2_test_transfer_from_self(ledger_env: impl LedgerEnv) -> anyhow
     )
     .await??;
 
-    // Transfered fee and payed additional fee, balance is now 0.
+    // Transfered fee and payed fee, balance is now 0.
     assert_balance(&ledger_env, p1_env.principal(), 0).await?;
     // Beneficiary should get the amount transferred.
     assert_balance(&ledger_env, p2_env.principal(), transfer_amount).await?;
